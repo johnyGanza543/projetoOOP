c4d9dd3252acfc66442bb63daedea8d7cfb8061b
johnyGanza543 <119758873+johnyGanza543@users.noreply.github.com>
2022-12-03 19:37:45 +0000

    Create main.java

---
+++ main.java
@@ -0,0 +1,22 @@
+package projetoPOO;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class main {
+    public static void main(String[] args) {
+        
+    }
+}
+
+public class lerFicheiro{
+    public static void main(String[] args){
+        throws IOException{
+            List<String> ficheiroNotas = new ArrayList<String>();
+            Scanner scanner = new Scanner(new FileReader(notas.txt));
+
+
+        }
+    }
+}
