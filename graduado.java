218abf2dd3b368628d63652d75647aa585190c6c
johnyGanza543 <119758873+johnyGanza543@users.noreply.github.com>
2022-12-03 19:40:16 +0000

    Update graduado.java

---
+++ graduado.java
@@ -0,0 +1,28 @@
+public class graduado extends Aluno {
+    protected Double primeiroTeste, segundoTeste;
+
+    public graduado (int numAluno, String nomeAluno){
+        super(numAluno, nomeAluno);
+        this.primeiroTeste = primeiroTeste;
+        this.segundoTeste = segundoTeste;
+    }
+
+    public int getprimeiroTeste(){
+        return primeiroTeste;
+    }
+    public void setprimeiroTeste(int primeiroTeste){
+        this.primeiroTeste = primeiroTeste;
+    }
+
+    public int getsegundoTeste(){
+        return primeiroTeste;
+    }
+    public void setsegundoTeste(int segundoTeste){
+        this.segundoTeste = segundoTeste;
+    }
+
+    @Override
+    public String toString(){
+        return // 
+    }
+}
