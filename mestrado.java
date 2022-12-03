751f2a462f062c919b55e01d9006433f680d75a7
johnyGanza543 <119758873+johnyGanza543@users.noreply.github.com>
2022-12-03 19:40:42 +0000

    Update mestrado.java

---
+++ mestrado.java
@@ -0,0 +1,10 @@
+public class mestrado extends Aluno{
+    public mestrado(int numAluno, String nomeAluno){
+        super(numAluno, nomeAluno);
+        }
+
+        @Override
+        public String toString(){
+            return //
+        }
+    }
